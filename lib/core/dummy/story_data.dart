4b2a950707b051f2593282c5fe599346908fb769
abdulrahmanahme <abdelramanahmed00@gmail.com>
2023-02-22 02:28:13 +0200

    Dream

--- lib/core/dummy/story_data.dart
+++ lib/core/dummy/story_data.dart
@@ -1,13 +1,10 @@
 import '../../model/model_story.dart';
 
-List <Story>storyData=[
-  Story(name:'جيلى', image: 'assets/images/Image 1.png'),
-  Story(name:'بى ام دبيو', image: 'assets/images/Image 5.png'),
-  Story(name:'تيوتا', image: 'assets/images/Image 6.png'),
-  Story(name:'جيلى', image: 'assets/images/Image 1.png'),
-  Story(name:'جيلى', image: 'assets/images/Image 11.png'),
-  Story(name:'جيلى', image: 'assets/images/Image 1.png'),
-  Story(name:'جيلى', image: 'assets/images/Image 1.png'),
-
-
+List<Story> storyData = [
+  Story(name: 'جيلى', image: 'assets/images/Image 1.png'),
+  Story(name: 'تيوتا', image: 'assets/images/Image 6.png'),
+  Story(name: 'بى ام دبيو', image: 'assets/images/Image 5.png'),
+  Story(name: 'جيلى', image: 'assets/images/Image 11.png'),
+  Story(name: 'بى ام دبيو', image: 'assets/images/Image 5.png'),
+  Story(name: 'تيوتا', image: 'assets/images/Image 6.png'),
 ];
